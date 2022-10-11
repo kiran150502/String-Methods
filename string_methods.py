55278972a244fa68c83ca6e9e825059be8d5ca99
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-10-11 23:07:36 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+print(len("python")) 
+
+a="python"
+print(a,type(a))
+print(a[0])
+
+print("python"[0]) 
+print("python"[1]) 
+print("python"[2]) 
+print("python"[3]) 
+print("python"[4]) 
+print("python"[5]) 
+print("python"[6]) 
